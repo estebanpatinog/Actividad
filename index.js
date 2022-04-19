7c10e1d289d00d9db7e189440df8aa9dae65ccd5
estebanpatinog <esteban.patinog@autonoma.edu.co>
2022-04-19 00:20:54 -0500

    Actividad Whatsapp

--- index.js
+++ index.js
@@ -16,6 +16,16 @@ const accountSid = process.env.TWILIO_ACCOUNT_SID;
 const authToken = process.env.TWILIO_AUTH_TOKEN;
 const client = require('twilio')(accountSid, authToken);
 
+//Actividad de whatsapp
+client.messages
+  .create({
+    mediaUrl: ['https://aldawe.co/wp-content/uploads/2018/12/colombia-1460312_1920.jpg'],
+     from: 'whatsapp:+14155238886',
+     body: 'Actividad de Whatsapp',
+     to: 'whatsapp:+573215702977'
+   })
+  .then(message => console.log(message.sid));
+
 const email = require('./src/Mail')
 const sgMail = require('@sendgrid/mail')
 sgMail.setApiKey(process.env.SENDGRID_API_KEY)
@@ -44,9 +54,6 @@ app.use((err, req, res, next)=>{
   res.status(statusCode).json({'message': err.message})
   return
 })
-
-
-
 
 
 app.use(express.json());
